d2bbc49171b8423cd4b5690b05bb34bd9a5f47a8
adi.wu <adi.wu@kiwigame.com>
2024-07-24 13:12:40 +0800

    Initial Commit

---
+++ spinup/spinupApp.swift
@@ -0,0 +1,17 @@
+//
+//  spinupApp.swift
+//  spinup
+//
+//  Created by adi on 2024/7/24.
+//
+
+import SwiftUI
+
+@main
+struct spinupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
